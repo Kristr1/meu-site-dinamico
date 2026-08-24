c82ad99d78a1cdd5af7f14fff771954f3b8fd130
Kristr1 <kristianbonchev@gmail.com>
2026-08-24 15:41:11 +0000

    aaaa

--- server.js
+++ server.js
@@ -3,6 +3,11 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Redireciona /index.html para a raiz /
+app.get('/index.html', (req, res) => {
+  res.redirect(301, '/');
+});
+
 // Servir os ficheiros estáticos da pasta public
 app.use(express.static(path.join(__dirname, 'public')));
 
